f900be2cde9e708dafc94fc0695077a3ba7928e6
NCKiser <noahkiser95mygc@gmail.com>
2023-11-05 20:44:09 -0600

    added capsword

--- keyboards/kiserdesigns/smidge/rules.mk
+++ keyboards/kiserdesigns/smidge/rules.mk
@@ -11,3 +11,4 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # We don't have BT
 RGBLIGHT_ENABLE = no        # We don't have underglow
 TAP_DANCE_ENABLE = yes
+CAPS_WORD_ENABLE = yes

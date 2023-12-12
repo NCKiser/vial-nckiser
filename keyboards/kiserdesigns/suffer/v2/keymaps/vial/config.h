bf8b5a082c468679243944e5828e377c7645a085
NCKiser <noahkiser95mygc@gmail.com>
2023-12-12 09:13:27 -0600

    add suffer v2, still wip

---
+++ keyboards/kiserdesigns/suffer/v2/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x0E, 0x2B, 0xCD, 0x9D, 0x2C, 0x60, 0xDC, 0xC5}
+
+#define VIAL_TAP_DANCE_ENTRIES 24
+
+#define VIAL_COMBO_ENTRIES 64
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 12
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 4 }

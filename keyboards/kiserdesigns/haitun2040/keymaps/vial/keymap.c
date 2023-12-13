a25a7be3cb10605c7db8805b93e6b6cfd62979cb
NCKiser <noahkiser95mygc@gmail.com>
2023-12-12 22:32:29 -0600

    add more layers, brightness follow

--- keyboards/kiserdesigns/haitun2040/keymaps/vial/keymap.c
+++ keyboards/kiserdesigns/haitun2040/keymaps/vial/keymap.c
@@ -15,38 +15,48 @@
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
   if (rgb_matrix_is_enabled()){
-    if (host_keyboard_led_state().caps_lock) {
-        switch (get_highest_layer(layer_state|default_layer_state)) {
-            case 1:
-                rgb_matrix_set_color(0, RGB_YELLOW);
-                break;
-            case 2:
-                rgb_matrix_set_color(0, RGB_MAGENTA);
-                break;
-            case 3:
-                rgb_matrix_set_color(0, RGB_WHITE);
-                break;
-            default: //  for any other layers, or the default layer
-                rgb_matrix_set_color(0, RGB_RED);
-                break;
-            };
-    };
+    HSV hsv = {0, 255, 0};
     if (!host_keyboard_led_state().caps_lock) {
         switch (get_highest_layer(layer_state|default_layer_state)) {
             case 1:
-                rgb_matrix_set_color(0, RGB_GREEN);
+                hsv.h = 0;
+                hsv.v = 255;
                 break;
             case 2:
-                rgb_matrix_set_color(0, RGB_BLUE);
+                hsv.h = 85;
+                hsv.v = 255;
                 break;
             case 3:
-                rgb_matrix_set_color(0, RGB_CYAN);
+                hsv.h = 171;
+                hsv.v = 255;
+                break;
+            case 4:
+                hsv.h = 43;
+                hsv.v = 255;
+                break;
+            case 5:
+                hsv.h = 128;
+                hsv.v = 255;
+                break;
+            case 6:
+                hsv.h = 213;
+                hsv.v = 255;
                 break;
             default: //  for any other layers, or the default layer
-                rgb_matrix_set_color(0, RGB_BLACK);
+                hsv.h = 0;
+                hsv.v = 0;
                 break;
             };
     }
+    if (host_keyboard_led_state().caps_lock) {
+        hsv.s = 0;
+        hsv.v = 255;
+    };
+    if (hsv.v > rgb_matrix_get_val()) {
+        hsv.v = rgb_matrix_get_val();
+    }
+    RGB rgb = hsv_to_rgb(hsv);
+    rgb_matrix_set_color(0, rgb.r, rgb.g, rgb.b);
   }
 	return true;
 }

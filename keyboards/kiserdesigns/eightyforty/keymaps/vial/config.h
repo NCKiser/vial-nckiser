71814bc4fd986124de638334709b27f80cc50f1d
NCKiser <noahkiser95mygc@gmail.com>
2023-12-13 00:23:07 -0600

    add eightyforty

---
+++ keyboards/kiserdesigns/eightyforty/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xDA, 0xF7, 0x3A, 0xD9, 0xE3, 0x1E, 0x1C, 0xE0}
+
+#define VIAL_TAP_DANCE_ENTRIES 32
+
+#define VIAL_COMBO_ENTRIES 32
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 16
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 5 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 4 }

74da71b991a9133ddee80cde1fbd7c291c0931cd
NCKiser <noahkiser95mygc@gmail.com>
2024-03-14 16:44:01 -0500

    Add LaTeX macropad

---
+++ keyboards/kiserdesigns/latexpad/keymaps/vial/config.h
@@ -0,0 +1,27 @@
+ /* This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 2 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA3, 0x3E, 0x0C, 0xBC, 0xCF, 0xD6, 0xF1, 0xEA}
+
+#define VIAL_TAP_DANCE_ENTRIES 32
+
+#define VIAL_COMBO_ENTRIES 32
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 10 }
+
+
